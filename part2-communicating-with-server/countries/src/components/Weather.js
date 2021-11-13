2aaa0b2445b2392c0911adbf41847a92e3cd06c7
Dan Curilla <curilladaniel@gmail.com>
2021-11-13 07:29:48 -0700

    Clean up comments

--- part2-communicating-with-server/countries/src/components/Weather.js
+++ part2-communicating-with-server/countries/src/components/Weather.js
@@ -1,13 +1,3 @@
-/*
-This component is currently throwing an error because nothing is returned
-to be rendered. I think the issue is it needs to return something right away,
-and then re-render when the state has been changed.
-
-Next Steps:
-1. Create state for the Weather component, or ... the Country component
-2. Apply useEffect() to gather the data post render and ultimately change the state
-3. Then add the component to the App.js file
-*/
 const Weather = ({ weather }) => {
   if (weather !== undefined) {
     return (
